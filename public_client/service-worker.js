0fc4e1a3a0ac39905ac8bf10d1731d885970df99
Pradeep c <pradeepc@Pradeeps-MacBook-Air.local>
2026-09-22 11:36:20 +0530

    fix: allow Render and Vercel in CORS and add global JSON error handler

---
+++ public_client/service-worker.js
@@ -0,0 +1,13 @@
+// Service worker disabled to prevent local Electron asset caching issues.
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    })
+  );
+  self.clients.claim();
+});
